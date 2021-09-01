41a56b8ce68b9047d8c98546d2c3d86017c2e8da
Mateusz Kapeluszny <mateusz_kapeluszny@tvn.pl>
2021-09-01 20:17:14 +0200

    gitignore

--- client-app/src/types.ts
+++
@@ -1,9 +0,0 @@
-export type Optional<T = string> = T | undefined | null;
-
-export interface BaseCalendarEvent {
-    id?: Optional;
-    name?: Optional;
-    description?: Optional;
-    dateStart?: Optional;
-    dateEnd?: Optional;
-}
