d439c7a90d153238c2916a1a4179a4de3c53ef11
Mateusz Kapeluszny <mateusz_kapeluszny@tvn.pl>
2021-09-01 20:18:22 +0200

    gitignore

--- generators/node_modules/@graphql-codegen/plugin-helpers/getCachedDocumentNodeFromSchema.d.ts
+++
@@ -1,2 +0,0 @@
-import { GraphQLSchema, DocumentNode } from 'graphql';
-export declare function getCachedDocumentNodeFromSchema(schema: GraphQLSchema): DocumentNode;
--- generators/node_modules/@graphql-codegen/plugin-helpers/index.d.ts
+++
@@ -1,7 +0,0 @@
-export { resolveExternalModuleAndFn } from './resolve-external-module-and-fn';
-export * from './types';
-export * from './utils';
-export * from './helpers';
-export * from './federation';
-export * from './errors';
-export * from './getCachedDocumentNodeFromSchema';
